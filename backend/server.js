9028cc56a900b4eb5d279f9660669863ed44f266
divyanshu00201 <divyanshuj.cs19@bmsce.ac.in>
2021-09-19 19:19:53 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,38 @@
 const express = require("express");
-const notes = require('./data/notes');
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const { notFound, errorHandler } = require("./middelware/errorMiddleware");
+const path = require("path");
 
-const app=express();
+const app = express();
 dotenv.config();
+connectDB();
+app.use(express.json());
 
-app.get("/", (req,res) => {
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
+
+// --------------------------deployment------------------------------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
     res.send("API is running..");
-});
+  });
+}
+// --------------------------deployment------------------------------
 
-app.get('/api/notes',(req,res) =>{
-    res.json(notes);
-});
-
-app.get('/api/notes/:id',(req,res)=>{
-    const note=notes.find((n)=>n._id===req.params.id);
-    res.send(note);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
